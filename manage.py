502532bdc76f58365864e7b9004b2b87c9049992
Alexei Bastidas <bastidas.alexei@gmail.com>
2016-06-22 22:09:23 -0400

    deployed

--- manage.py
+++ manage.py
@@ -12,7 +12,7 @@ from app.models import User, Comment
 from flask_script import Manager, Shell, Server
 from flask_migrate import Migrate, MigrateCommand
 
-app = create_app('default')
+app = create_app('production')
 
 manager = Manager(app)
 migrate = Migrate(app,db)
@@ -23,7 +23,6 @@ def make_shell_context():
 @manager.command
 def start_db():
     import csv
-    os.remove("data-dev.sqlite")
     db.create_all()
     with open('reddit_downvoted_15k.csv') as csvfile:
         file = csv.reader(csvfile, delimiter=',', quotechar='"')
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+from app import *
+
+application = create_app('production')
